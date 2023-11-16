ad271e44855c4c0d18693de849e1f1782f9f57fb
mini604 <deeptimishra604@gmail.com>
2023-11-16 07:41:20 -0800

    distance between the first 100 prime numbers and sum of first 100 prime numbers

---
+++ JavaScript_Basic/DistanceBetweenPrime.js
@@ -0,0 +1,45 @@
+function generatePrimes(count) {
+    const primes = [];
+    let num = 2;
+  
+    while (primes.length < count) {
+      if (isPrime(num)) {
+        primes.push(num);
+      }
+      num++;
+    }
+  
+    return primes;
+  }
+  
+  function isPrime(number) {
+    if (number <= 1) {
+      return false;
+    }
+  
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false; // The number is divisible, hence not prime
+      }
+    }
+  
+    return true; // The number is prime
+  }
+  
+  function calculateDistances(primes) {
+    const distances = [];
+  
+    for (let i = 1; i < primes.length; i++) {
+      distances.push(primes[i] - primes[i - 1]);
+    }
+  
+    return distances;
+  }
+  
+  // Example usage:
+  const primes = generatePrimes(100);
+  const distances = calculateDistances(primes);
+  
+  console.log("The first 100 prime numbers are:", primes);
+  console.log("The distances between consecutive prime numbers are:", distances);
+  
---
+++ JavaScript_Basic/PrimeSumArray.js
@@ -0,0 +1,39 @@
+function generatePrimes(count) {
+    const primes = [];
+    let num = 2;
+  
+    while (primes.length < count) {
+      if (isPrime(num)) {
+        primes.push(num);
+      }
+      num++;
+    }
+  
+    return primes;
+  }
+  
+  function isPrime(number) {
+    if (number <= 1) {
+      return false;
+    }
+  
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false; // The number is divisible, hence not prime
+      }
+    }
+  
+    return true; // The number is prime
+  }
+  
+  function sumOfFirstNPrimes(n) {
+    const primes = generatePrimes(n);
+    const sum = primes.reduce((acc, curr) => acc + curr, 0);
+    return { primes, sum };
+  }
+  
+  // Example usage:
+  const result = sumOfFirstNPrimes(100);
+  console.log("The first 100 prime numbers are:", result.primes);
+  console.log("The sum of the first 100 prime numbers is:", result.sum);
+  
