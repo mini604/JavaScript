c803fa2b99db47604179e27a113021793d8f7540
mini604 <deeptimishra604@gmail.com>
2023-11-17 16:06:26 -0800

    weather report completed

---
+++ JavaScript_Basic/Weather/Script.js
@@ -0,0 +1,33 @@
+function getWeather() {
+    // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
+    const apiKey = 'ebd909def32d427d00b08b0af109ded3';
+    const cityInput = document.getElementById('search').value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherResult = document.getElementById('weatherResult');
+
+            if (data.cod === '404') {
+                // City not found
+                weatherResult.innerHTML = `<p>City not found. Please enter a valid city name.</p>`;
+            } else {
+                // Display weather information
+                const weatherDescription = data.weather[0].description;
+                const temperature = data.main.temp;
+                const cityName = data.name;
+
+                weatherResult.innerHTML = `
+                <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+                    <p>Weather in ${cityName}: ${weatherDescription}</p>
+                    <h2>Temperature: ${temperature} &#8451;</h2>
+                `;
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
